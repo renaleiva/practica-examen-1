70fbefbcfc302ea447155cf23bb5391f0fd86640
renaleiva <rena.leiva@duocuc.cl>
2026-07-08 13:37:11 -0400

    agregamos el menu para posteriormente mostrarlo al usuario y que eliga una opcion

--- practica1.py
+++ practica1.py
@@ -1,2 +1,13 @@
 lista_canciones = []
-print("hola mundo")
+
+
+def mostar_menu():
+    print("======MENU PRINCIPAL======")
+    print("1. Agregar cancion")
+    print("2. Buscar cancion")
+    print("3. Eliminar cancion")
+    print("4. Actualizar estados")
+    print("5. Mostrar canciones")
+    print("6. Salir")
+
+    
